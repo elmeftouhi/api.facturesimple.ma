c2b402897d3ac3dc6bd0862e493f4bf7c86450bf
Yassine ELMEFTOUHI <Yassine.Elmeftouhi.ext@speos.be>
2026-06-18 14:31:07 +0200

    first commit

---
+++ src/main/java/com/elmeftouhi/facturesimple/FacturesimpleApplication.java
@@ -0,0 +1,13 @@
+package com.elmeftouhi.facturesimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturesimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturesimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elmeftouhi/facturesimple/FacturesimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmeftouhi.facturesimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturesimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
